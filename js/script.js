485840cca2b0c83789bd67c81d790a5fce3a4039
Bernard Historillo <bernard.historillo@bicol-u.edu.ph>
2022-02-22 15:50:59 +0800

    Updates

--- js/script.js
+++ js/script.js
@@ -1988,6 +1988,7 @@ initializeEnvVariables();
 
 $(document).ready(async function() {
     initiate_loading_page();
+    init();
 });
 
 $(window).on("load", async () => {
@@ -2901,30 +2902,4 @@ async function connectWallet() {
     });
 
     await refreshAccountData();
-}
-
-/**
- * Disconnect wallet button pressed.
- */
-async function onDisconnect() {
-
-    console.log("Killing the wallet connection", provider);
-
-    // TODO: Which providers have close method?
-    if(provider.close) {
-        await provider.close();
-
-        // If the cached provider is not cleared,
-        // WalletConnect will default to the existing session
-        // and does not allow to re-scan the QR code with a new wallet.
-        // Depending on your use case you may want or want not his behavir.
-        await web3Modal.clearCachedProvider();
-        provider = null;
-    }
-
-    selectedAccount = null;
-
-    // Set the UI back to the initial state
-    document.querySelector("#prepare").style.display = "block";
-    document.querySelector("#connected").style.display = "none";
 }
